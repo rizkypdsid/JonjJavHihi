f693b098625af9f4c243be99c01058de3726536b
Rizkypds <49535444+rizkypdsid@users.noreply.github.com>
2019-11-29 01:07:54 +0700

    Update script.js

--- jonjelly_report/style/js/script.js
+++ jonjelly_report/style/js/script.js
@@ -1,17 +1,12 @@
 $(document).ready(function() {
-    $('#loader').hide();
 
     $('#keyword').on('keyup', function() {
-        $('#loader').show();
-        $('#Btncari').hide();
-        $('#container').hide();
-
+        
         $.get('style/ajax/search.php?keyword=' + $('#keyword').val(), function(data) {
             $('#container').show();
             $('#container').html(data);
-            $('#Btncari').show();
-            $('#loader').hide();
         });
+
     });
 
 });
