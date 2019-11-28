cc609a5aae40c9dba7e8cb2bab197009912ebc1f
Rizkypds <49535444+rizkypdsid@users.noreply.github.com>
2019-11-29 01:04:59 +0700

    Update CustomerList.php

--- jonjelly_report/CustomerList.php
+++ jonjelly_report/CustomerList.php
@@ -1,7 +1,10 @@
 <?php
 include "config/database.php";
-$CustList = mysqli_query($connection,
+$getCustList = mysqli_query($connection,
                                         "SELECT DISTINCT id_pesanan, nama_cust, total_pembayaran, tangga_beli FROM pesanan ORDER BY id_pesanan DESC");
+
+$getListPayment = mysqli_query($connection, 
+                                        "SELECT tangga_beli AS TANGGAL, SUM(DISTINCT total_pembayaran) AS TOTAL FROM pesanan GROUP BY tangga_beli ORDER BY tangga_beli DESC");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -24,84 +27,114 @@ $CustList = mysqli_query($connection,
             <div class="col col-3 ">
                 <img src="img/icon-png.png" alt="" width="60rem;" class="float-left mr-2 d-print-none">
                 <h1 class="text-black-50 BrandTitle">JONJELLY</h1>
-                <h2 class="BrandDesc text-black-50">- Jebule Ora Nepu</h2>
+                <h2 class="text-black-50 BrandDesc">- Jebule Ora Nepu</h2>
             </div>
             <div class="mx-auto text-center">
-                <h2 class="text-black-50">Customer List</h2>
-               <p class="DescTitleLaprn text-black-50 text-capitalize">last customer service</p>
+                <h1 class="text-black-50 BrandTitle text-uppercase">Customer List</h1>
+                <h2 class="text-black-50 BrandDesc  text-uppercase">- last customer service</h2>
             </div>
-            <div class="col-3 text-right text-black-50">
+            <div class="col-3 text-right text-black-50 BrandTitle">
                 <?php 
                     date_default_timezone_set('Asia/Jakarta');
                     $jam=date("G:i:s a");
-                    echo date('l, d-M-Y')  . "<br>" .  $jam  
+                    echo date('l, d-M-Y')  . "<br>" .  $jam ;
                 ?>
             </div>
         </div>
-        <!-- End Header -->
-        <!-- Start Search And Back -->
-        <form autocomplete="off">
+        <!-- Start form -->
+        <form autocomplete="off" class="mt-2">
             <div class="form-group row d-print-none">
+                <!-- Start Button Header -->
                 <div class="col-sm-5 mr-auto">
                     <a href="index.php" class="btn btn-info BackToHome" title="back to home"></a>
-                    <a href="javascript:printpdf();" class="btn btn-info CustDownload" title="Download List Customer"></a>
+                    <a href="javascript:printpdf();" class="btn btn-danger PrintList" title="Download List"></a>
+                    <a href="CustExcel.php" class="btn btn-success ExportList" title="Download List"></a>
                 </div>
+                <!-- End Button Header -->
                 <div class="col-sm-5 ml-auto">
                 <input type="text" class="form-control" name="keyword" id="keyword" placeholder="please insert id faktur..." >
                 </div>
                 <button type="submit"  name="cari" id="Btncari" class="btn btn-primary" disabled style="height:38px;"><i class="fas fa-search"></i> Search</button>
             </div>
         </form>
-        <div class="row">
-            <div class="col-sm-3 mx-auto d-print-none">
-            <img src="img/loader.gif" alt=""  style="width: 250px;" id="loader">
-            </div>
-        </div>
-        <!-- End Search And Back -->
+        <!-- End form -->
+        <!-- End Header -->
         <!-- Start Content Table Data -->
         <div class="row mt-4">
+            <!-- Start Table Last Customer -->
             <div class="col col-lg-12">
                 <div id="container">
-                <table class="table table-striped mx-auto table-sm">
-                    <thead>
-                        <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">ID FAKTUR</th>
-                        <th scope="col">NAME</th>
-                        <th scope="col">SUBTOTAL</th>
-                        <th scope="col">DATE</th>
-                        <th scope="col" class="d-print-none">ACTION</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php if(mysqli_num_rows($CustList)>0){ ?>
-                        <?php
-                            $no = 1;
-                            while($data = mysqli_fetch_array($CustList)){
-                                $payment=number_format($data['total_pembayaran'],0,",",".");
-                        ?>
-                        <tr>
-                            <th scope="row"><?= $no ?></th>
-                            <td><?= $data["id_pesanan"];?></td>
-                            <td class="text-capitalize"><?= $data["nama_cust"];?></td>
-                            <td>Rp. <?= $payment ?></td>
-                            <td><?= $data["tangga_beli"];?></td>
-                            <td class="d-print-none">
-                                <a class="btn btn-info btn-sm" href="struk.php?id=<?= $data["id_pesanan"]; ?>" title="Print Struk">
-                                <i class="fas fa-print"></i> PRINT</a> 
-                            </td>
-                        </tr>
-                        <?php $no++; } ?>
-                        <?php } ?>
-                    </tbody>
-                </table>
+                    <table class="table table-striped mx-auto table-sm">
+                        <thead>
+                            <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">ID FAKTUR</th>
+                            <th scope="col">NAME</th>
+                            <th scope="col">SUBTOTAL</th>
+                            <th scope="col">DATE</th>
+                            <th scope="col" class="d-print-none">ACTION</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php if(mysqli_num_rows($getCustList)>0){ ?>
+                            <?php
+                                $no = 1;
+                                while($data = mysqli_fetch_array($getCustList)){
+                                    $CustList=number_format($data['total_pembayaran'],0,",",".");
+                            ?>
+                            <tr>
+                                <th scope="row"><?= $no ?></th>
+                                <td><?= $data["id_pesanan"];?></td>
+                                <td class="text-capitalize"><?= $data["nama_cust"];?></td>
+                                <td>Rp. <?= $CustList ?></td>
+                                <td><?= $data["tangga_beli"];?></td>
+                                <td class="d-print-none">
+                                    <a class="btn btn-info btn-sm" href="struk.php?id=<?= $data["id_pesanan"]; ?>" title="Print Struk">
+                                    <i class="fas fa-print"></i> PRINT</a> 
+                                </td>
+                            </tr>
+                            <?php $no++; } ?>
+                            <?php } ?>
+                        </tbody>
+                    </table>
                 </div>
             </div>
+            <!-- Start Table Last Customer -->
+            <!-- Start Table Daily Payment -->
+            <div class="col col-lg-12">
+                <div id="container">
+                    <table class="table table-striped mx-auto table-sm">
+                        <thead>
+                            <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">DAILY PAYMENT</th>
+                            <th>TOTAL</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php if(mysqli_num_rows($getListPayment)>0){ ?>
+                            <?php
+                                $no = 1;
+                                while($data = mysqli_fetch_array($getListPayment)){
+                                    $payment=number_format($data['TOTAL'],0,",",".");
+                            ?>
+                            <tr>
+                                <th  scope="row"><?= $no ?></th>
+                                <td><?= $data["TANGGAL"];?></td>
+                                <td>Rp. <?= $payment ?></td>
+                            </tr>
+                            <?php $no++; } ?>
+                            <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <!-- End Table Daily Payment -->
         </div>
         <!-- End Content Table Data -->
         <!-- Start Copyright  -->
         <div class="row">
-            <div class="col fixed-bottom text-black-50  d-print-inline">
+            <div class="col fixed-bottom text-black-50  d-none d-print-inline">
                 <hr>
             Copyright © 2019 - Jonjelly. All rights reserved.
             </div>
